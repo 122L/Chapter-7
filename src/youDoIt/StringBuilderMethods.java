222dd6ae2c181a09489d5843c1c8d97b15c21014
122L <og153182@students.davenport.k12.ia.us>
2020-02-07 12:59:51 -0600

    bruh

--- src/youDoIt/StringBuilderMethods.java
+++ src/youDoIt/StringBuilderMethods.java
@@ -1,4 +1,16 @@
 package youDoIt;
 
 public class StringBuilderMethods {
+    public static void main(String[] args) {
+        StringBuilder str = new StringBuilder("singing");
+        System.out.println(str);
+
+        str.append(" in the dead of ");
+        System.out.println(str);
+
+        str.insert(0, "Black ");
+        System.out.println(str);
+        str.insert(5, "bird ");
+        System.out.println(str);
+    }
 }
