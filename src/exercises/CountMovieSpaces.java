6051d8be7231cb0c89817df89a3c8109cd1bc306
122L <og153182@students.davenport.k12.ia.us>
2020-02-10 13:11:05 -0600

    bruh

--- src/exercises/CountMovieSpaces.java
+++ src/exercises/CountMovieSpaces.java
@@ -4,6 +4,21 @@ import java.util.Scanner;
 
 public class CountMovieSpaces {
     public static void main(String[] args) {
+        String Quote;
+        int numberOfSpace = 0;
 
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a movie quote >>> ");
+        Quote = input.nextLine();
+
+        for(int i = 0; i < Quote.length(); i++) {
+            if (Quote.charAt(i) ==  ' ') {
+                numberOfSpace++;
+                ++i;
+
+            }
+        }
+        System.out.println("Number of spaces >>> " + numberOfSpace);
     }
 }
---
+++ src/exercises/PigLatin.java
@@ -0,0 +1,18 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class PigLatin {
+    public static void main(String[] args) {
+        String words;
+        String ay = "ay";
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Insert words >>> ");
+        words = input.nextLine();
+
+        for(int i = 0; i < words.length();)
+
+    }
+}
